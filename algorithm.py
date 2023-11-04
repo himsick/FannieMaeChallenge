35bf12766090fc82b465e5f004c9308f99bfb05e
Him sick <himsick@gmail.com>
2023-11-04 17:53:00 -0500

    fourth

--- algorithm.py
+++ algorithm.py
@@ -10,21 +10,21 @@ df = pd.read_csv(file_path)
 df['CreditScoreCheck'] = df['CreditScore'] >= 640
  
 # Calculate the LTV ratio and determine status
-df['LTV'] = df['EstMonthlyMortgagePayment'] / df['HomeAppraisedValue']
+df['LTV'] = df['MonthlyMortgagePayment'] / df['AppraisedValue']
 df['LTV_Status'] = 'Undetermined'
 df.loc[df['LTV'] < 0.8, 'LTV_Status'] = 'Preferred'
 df.loc[(df['LTV'] >= 0.8) & (df['LTV'] <= 0.95), 'LTV_Status'] = 'Might Require Insurance'
 df.loc[df['LTV'] > 0.95, 'LTV_Status'] = 'High Risk'
  
 # Calculate the DTI ratio and determine status
-df['DTI'] = (df['MonthlyCarPayment'] + df['MonthlyCreditCardPayment'] + df['StudentLoanPayment'] + df['EstMonthlyMortgagePayment']) / df['GrossMonthlyIncome']
+df['DTI'] = (df['CarPayment'] + df['CreditCardPayment'] + df['StudentLoanPayments'] + df['MonthlyMortgagePayment']) / df['GrossMonthlyIncome']
 df['DTI_Status'] = 'Undetermined'
 df.loc[df['DTI'] <= 0.36, 'DTI_Status'] = 'Preferred'
 df.loc[(df['DTI'] > 0.36) & (df['DTI'] <= 0.43), 'DTI_Status'] = 'Acceptable'
 df.loc[df['DTI'] > 0.43, 'DTI_Status'] = 'Not Preferred'
  
 # Calculate the FEDTI ratio and determine status
-df['FEDTI'] = df['EstMonthlyMortgagePayment'] / df['GrossMonthlyIncome']
+df['FEDTI'] = df['MonthlyMortgagePayment'] / df['GrossMonthlyIncome']
 df['FEDTI_Status'] = 'Undetermined'
 df.loc[df['FEDTI'] <= 0.28, 'FEDTI_Status'] = 'Acceptable'
 df.loc[df['FEDTI'] > 0.28, 'FEDTI_Status'] = 'Not Preferred'
