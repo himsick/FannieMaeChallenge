f137da2b07510724eb56d2980c414c49c026129f
Him sick <himsick@gmail.com>
2023-11-05 01:41:42 -0600

    python working

--- algorithm.py
+++ algorithm.py
@@ -1,44 +1,49 @@
 import pandas as pd
-
-# Replace 'your_data.csv' with the path to your actual CSV file.
-file_path = 'HomeBuyer.csv'
-
-# Read the data from the CSV file
-df = pd.read_csv(file_path)
-
-# Check if the CreditScore for each buyer is 640 or above
-df['CreditScoreCheck'] = df['CreditScore'] >= 640
-
-# Calculate the LTV ratio and determine status
-df['LTV'] = df['LoanAmount'] / df['AppraisedValue']  # Assuming you have a LoanAmount column
+ 
+# Sample user input received from the React app
+user_input = {
+    'creditScore': 680,
+    'loanAmount': 200000,
+    'appraisedValue': 250000,
+    'grossMonthlyIncome': 5000,
+    'totalMonthlyPayments': 800,
+    'monthlyMortgagePayment': 1200,
+}
+ 
+# Convert user input to a DataFrame
+df = pd.DataFrame([user_input])
+ 
+# Credit Score Check
+df['CreditScoreCheck'] = df['creditScore'] >= 640
+ 
+# LTV Ratio Calculation
+df['LTV'] = df['loanAmount'] / df['appraisedValue']
 df['LTV_Status'] = 'Undetermined'
 df.loc[df['LTV'] < 0.8, 'LTV_Status'] = 'Preferred'
 df.loc[(df['LTV'] >= 0.8) & (df['LTV'] <= 0.95), 'LTV_Status'] = 'Might Require Insurance'
 df.loc[df['LTV'] > 0.95, 'LTV_Status'] = 'High Risk'
-
-# Calculate the DTI ratio and determine status
-df['DTI'] = (df['CarPayment'] + df['CreditCardPayment'] + df['StudentLoanPayments'] + df['MonthlyMortgagePayment']) / df['GrossMonthlyIncome']
+ 
+# DTI Ratio Calculation
+df['DTI'] = df['totalMonthlyPayments'] / df['grossMonthlyIncome']
 df['DTI_Status'] = 'Undetermined'
 df.loc[df['DTI'] <= 0.36, 'DTI_Status'] = 'Preferred'
 df.loc[(df['DTI'] > 0.36) & (df['DTI'] <= 0.43), 'DTI_Status'] = 'Acceptable'
 df.loc[df['DTI'] > 0.43, 'DTI_Status'] = 'Not Preferred'
-
-# Calculate the FEDTI ratio and determine status
-df['FEDTI'] = df['MonthlyMortgagePayment'] / df['GrossMonthlyIncome']
+ 
+# FEDTI Ratio Calculation
+df['FEDTI'] = df['monthlyMortgagePayment'] / df['grossMonthlyIncome']
 df['FEDTI_Status'] = 'Undetermined'
 df.loc[df['FEDTI'] <= 0.28, 'FEDTI_Status'] = 'Acceptable'
 df.loc[df['FEDTI'] > 0.28, 'FEDTI_Status'] = 'Not Preferred'
-
-# Filter the DataFrame to only include entries where all three checks are 'Preferred' or 'Acceptable'
-filtered_df = df[
-    (df['CreditScoreCheck']) &
-    (df['LTV_Status'].isin(['Preferred', 'Might Require Insurance'])) &
-    (df['DTI_Status'].isin(['Preferred', 'Acceptable'])) &
-    (df['FEDTI_Status'] == 'Acceptable')
-]
-
-# Display the result for the filtered DataFrame
-print(filtered_df[['ID', 'CreditScore', 'CreditScoreCheck', 'LTV', 'LTV_Status', 'DTI', 'DTI_Status', 'FEDTI', 'FEDTI_Status']])
-
-# Save the result back to CSV if needed
-# filtered_df.to_csv('processed_data_filtered.csv', index=False)
+ 
+# Compile the response
+response = {
+    'CreditScoreCheck': 'Pass' if df.iloc[0]['CreditScoreCheck'] else 'Fail',
+    'LTV_Status': df.iloc[0]['LTV_Status'],
+    'DTI_Status': df.iloc[0]['DTI_Status'],
+    'FEDTI_Status': df.iloc[0]['FEDTI_Status'],
+}
+ 
+# Print the response (or you can return this in your Flask API)
+print(response)
+ 
