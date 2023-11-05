fadbc0952c2880fb70a015fc9e04dac0a510bb28
Omar Ahmad <talk2omar55@gmail.com>
2023-11-04 22:26:07 -0500

    Added the NavBar and major UI elements

---
+++ proportunity/src/App.jsx
@@ -0,0 +1,23 @@
+// Setup
+import React, { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+//import Faq from './components/Faq';
+//import Hero from './components/Hero';
+//import Form from './components/Form';
+//import Chatbot from './components/Chatbot';
+//import Testimonial from './components/Testimonial';
+//import Footer from './components/Footer';
+
+// App Function
+const App = () =>
+{
+  return(
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+// Final Export of App
+export default App;
